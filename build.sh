e68779adbbeae4ee041c9ab4c703c44fbbb2cb05
Arvin <arvin85113@gmail.com>
2026-05-14 03:03:41 +0000

    chore: run verify_existing_users on deploy to avoid locking out existing users

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py ensure_superuser
+python manage.py verify_existing_users
 python manage.py collectstatic --noinput
 python manage.py fix_empty_slugs
